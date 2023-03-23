ead802323a0c896529d2497ba63ee119bb2573e7
luisrodriguezgalvez <luis.rodriguez@deltares.nl>
2023-03-23 15:18:50 +0100

    Update MapboxMap.vue
    added default map center and zoom

--- src/components/MapboxMap.vue
+++ src/components/MapboxMap.vue
@@ -4,9 +4,12 @@
       class="mapbox-map"
       access-token="pk.eyJ1Ijoic2lnZ3lmIiwiYSI6Il8xOGdYdlEifQ.3-JZpqwUa3hydjAJFXIlMA"
       map-style="mapbox://styles/mapbox/streets-v11"
+      :center="mapCenter"
+      :zoom="mapZoom"
       id="map"
       ref="map"
     >
+      <v-mapbox-navigation-control :position="'bottom-right'" />
       <v-mapbox-layer @click='pointClicked' v-if="layerLocations" :options="layerLocations" :clickable="true"></v-mapbox-layer>
     </v-mapbox>
   </div>
@@ -18,6 +21,12 @@ export default {
   name: 'MapboxMap',
   mounted () {
     this.map = this.$refs.map.map
+  },
+  data () {
+    return {
+      mapCenter: [4.6, 52.435],
+      mapZoom: 12
+    }
   },
   computed: {
     ...mapState(['layerLocations'])
