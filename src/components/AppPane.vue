499922070bf8f9a57fd9d0c73090978aa464ea3d
Lucia Sarni <lucia@voorhoede.nl>
2023-03-31 16:31:47 +0200

    using dummy pie chart

--- src/components/AppPane.vue
+++ src/components/AppPane.vue
@@ -53,18 +53,19 @@
     </v-card>
         <!-- <area-chart v-if="showChart" /> -->
       </div>
-
+      <v-chart :option="chartOptions" />
       </div>
     </div>
 </template>
 
 <script>
-
+import 'echarts'
+import VChart from 'vue-echarts'
 import LocationDetails from './LocationDetails'
 
 export default {
   name: 'AppPane',
-  components: { LocationDetails },
+  components: { LocationDetails, VChart },
   props: {
     panelIsCollapsed: {
       type: Boolean,
@@ -73,6 +74,56 @@ export default {
     pointSelected: {
       type: Object,
       default: () => ({})
+    }
+  },
+  data () {
+    return {
+      chartOptions: {
+        textStyle: {
+          fontFamily: 'Inter, "Helvetica Neue", Arial, sans-serif'
+        },
+        title: {
+          text: 'Traffic Sources',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: [
+            'Direct',
+            'Email',
+            'Ad Networks',
+            'Video Ads',
+            'Search Engines'
+          ]
+        },
+        series: [
+          {
+            name: 'Traffic Sources',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              { value: 335, name: 'Direct' },
+              { value: 310, name: 'Email' },
+              { value: 234, name: 'Ad Networks' },
+              { value: 135, name: 'Video Ads' },
+              { value: 1548, name: 'Search Engines' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
     }
   },
   computed: {
